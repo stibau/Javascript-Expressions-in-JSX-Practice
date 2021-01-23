598e14d04202f3927efda009e2459629c4477918
stibau <48186853+stibau@users.noreply.github.com>
2021-01-23 16:30:00 +0100

    exercise solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name="Stef";
 
-ReactDOM.render(,)
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {new Date().getFullYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
+
