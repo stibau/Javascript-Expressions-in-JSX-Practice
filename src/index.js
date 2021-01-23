a8556b899db22018523238923706d48a45089d51
stibau <48186853+stibau@users.noreply.github.com>
2021-01-23 16:38:16 +0100

    exercise solution

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name="Stef";
+const name = "Stef";
+const currentDate = new Date();
+console.log(currentDate);
+const currentYear = currentDate.getFullYear();
 
 ReactDOM.render(
   <div>
     <p>Created by {name}</p>
-    <p>Copyright {new Date().getFullYear()}</p>
+    <p>Copyright {currentYear}</p>
   </div>,
   document.getElementById("root")
 );
-
